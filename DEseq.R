841390083b89f7c551691072a95d4b24cf7f0c32
dominikkaufmann3 <dominik.kaufmann3@students.unibe.ch>
2025-12-02 13:57:41 +0100

    Add files via upload

---
+++ DEseq.R
@@ -0,0 +1,95 @@
+
+# Exploratory data analysis
+
+
+# Load libraries
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("DESeq2")
+
+library(DESeq2)
+library(ggplot2)
+library(pheatmap)
+
+# 1. Import count matrix
+
+# Read counts (skip first line if it's a comment)
+counts <- read.table("counts_clean.txt", header=TRUE, row.names=1, check.names=FALSE)
+
+# Strip paths from column names
+colnames(counts) <- sub(".*/(SRR[0-9]+)\\.sorted\\.bam", "\\1", colnames(counts))
+
+# Now the column names are just sample IDs
+colnames(counts)
+
+dim(counts)
+head(counts[,1:6])
+
+# 2. Define sample metadata
+
+sample_info <- data.frame(
+  row.names = colnames(counts),
+  genotype = c(
+    "WT","WT","WT","WT","WT",
+    "DKO","DKO","DKO","DKO",
+    "WT","WT","WT",
+    "DKO","DKO","DKO"
+  ),
+  infection = c(
+    "Case","Case","Case","Case","Case",
+    "Case","Case","Case","Case",
+    "Control","Control","Control",
+    "Control","Control","Control"
+  )
+)
+
+
+# Verify order
+all(rownames(sample_info) == colnames(counts))
+
+# 3. Create DESeqDataSet
+
+dds <- DESeqDataSetFromMatrix(
+  countData = counts,
+  colData = sample_info,
+  design = ~ genotype + infection
+)
+
+# 4. Run DESeq normalization & modeling
+
+dds <- DESeq(dds)
+
+# 5. Variance-stabilizing transformation
+
+vsd <- vst(dds, blind = TRUE)
+
+# 6. PCA plot
+# PCA plot
+p <- plotPCA(vsd, intgroup = c("genotype", "infection"))  # ggplot object
+print(p)
+
+# Save PCA
+ggsave("PCA_plot.png", p, width = 6, height = 5)
+
+# PC1, PC2 values for each sample
+pca_data <- plotPCA(vsd, intgroup = c("genotype", "infection"), returnData = TRUE)
+pc1_table <- pca_data[, c("name", "PC1", "PC2")]
+print(pc1_table)
+
+
+
+
+# 7. Optional
+
+# vsd is the variance-stabilized DESeqDataSet
+
+# Perform PCA using prcomp directly on the assay
+pca <- prcomp(t(assay(vsd)))  # transpose so samples are rows
+
+# The loadings tells how much each gene contributes to each principal component
+loadings <- pca$rotation  # genes × PCs
+
+# Inspect top contributing genes for PC1
+top_PC1_genes <- head(sort(abs(loadings[,1]), decreasing = TRUE), 20)
+top_PC1_genes
+
